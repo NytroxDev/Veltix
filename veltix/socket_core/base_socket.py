3387ae72944a03ecf1ce3379e5a338d24d897650
NytroxDev <17164.fichier47@gmail.com>
2026-07-05 16:01:54 +0200

    refactor: change BaseSocket from Protocol to ABC

--- veltix/socket_core/base_socket.py
+++ veltix/socket_core/base_socket.py
@@ -1,7 +1,8 @@
-"""Base socket protocol for Veltix."""
+"""Base socket abstract class for Veltix."""
 
+from abc import ABC, abstractmethod
 from enum import Enum
-from typing import Callable, Protocol, Union, runtime_checkable
+from typing import Callable, Union
 
 from .managers.clients_manager import ClientEntry, ClientsManager
 
@@ -12,19 +13,35 @@ class SocketEvents(Enum):
     RECV = "recv"
 
 
-@runtime_checkable
-class BaseSocket(Protocol):
+class BaseSocket(ABC):
     client_manager: ClientsManager
     handshake_timeout: float
 
+    @abstractmethod
     def set_callback(self, event: SocketEvents, callback: Callable) -> bool: ...
+
+    @abstractmethod
     def send(self, data: bytes) -> bool: ...
+
+    @abstractmethod
     def close(self) -> bool: ...
+
+    @abstractmethod
     def bind(
         self, host: str, port: int, max_client: int, buffer_size: int, timeout: float
     ) -> bool: ...
+
+    @abstractmethod
     def connect(self, host: str, port: int, buffer_size: int, timeout: float) -> bool: ...
+
+    @abstractmethod
     def settimeout(self, timeout: float) -> bool: ...
+
+    @abstractmethod
     def close_client(self, client: Union[ClientEntry, int]) -> bool: ...
+
+    @abstractmethod
     def disconnect(self, timeout: float) -> bool: ...
+
+    @abstractmethod
     def recv(self, buf_size: int) -> bytes: ...
