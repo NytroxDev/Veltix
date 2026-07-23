8c58a326cdfaa07c4c5acb689cef245622666406
NytroxDev <17164.fichier47@gmail.com>
2026-07-23 18:59:26 +0200

    style: fix ruff formatting in logger_example.py

--- examples/logger_example.py
+++ examples/logger_example.py
@@ -6,9 +6,7 @@ Demonstrates standalone logger usage and bus-integrated logging.
 
 from veltix import Logger, LoggerConfig, LogLevel
 
-logger = Logger.get_instance(
-    LoggerConfig(level=LogLevel.DEBUG, show_timestamp=True)
-)
+logger = Logger.get_instance(LoggerConfig(level=LogLevel.DEBUG, show_timestamp=True))
 
 logger.trace("This is a trace message")
 logger.debug("This is a debug message")
