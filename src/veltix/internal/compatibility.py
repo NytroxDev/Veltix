82708454f56609626e908b27ea568c90ba5981c7
NytroxDev <17164.fichier47@gmail.com>
2026-08-11 17:02:41 +0200

    fix(compatibility): register v2.0.1 as compatible with v2.0.0

--- src/veltix/internal/compatibility.py
+++ src/veltix/internal/compatibility.py
@@ -121,6 +121,7 @@ class Version:
 # ---------------------------------------------------------------------------
 
 COMPATIBILITY: dict[Version, list[Version]] = {
+    Version(2, 0, 1): [Version(2, 0, 1), Version(2, 0, 0)],
     Version(2, 0, 0): [Version(2, 0, 0)],
     Version(1, 9, 0): [Version(1, 9, 0)],
     Version(1, 8, 1): [Version(1, 8, 1)],
